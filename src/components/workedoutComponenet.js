dd8324f40786e902659c3e63221f7cb432181410
gurudevssutar <gurudevssutar@gmail.com>
2021-10-24 22:04:00 +0530

    Ui changed

--- src/components/workedoutComponenet.js
+++ src/components/workedoutComponenet.js
@@ -5,10 +5,19 @@ import '../assets/scss/_supportive.scss'
 const WorkedOutComponent = (props) => {
     return (
         <div className="supportive">
+            <h4>{props.value.eg_no}</h4>
             <h1>{props.value.question}</h1>
             <h2>{props.value.sample_input}</h2>
-            <h2>{props.value.sample_output}</h2>
-            <h2>{props.value.ans}</h2>
+            {/* <p>{props.value.sample_output}</p> */}
+            <div dangerouslySetInnerHTML={{ __html: props.value.sample_output }}></div>
+            {props.value.ans && <iframe src={props.value.ans}
+                    style={{width:'100%', height:'500px', border:'0', borderRadius: '4px', overflow:'hidden'}}
+                    title="node-express-rest-template"
+                    allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+                    sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+                ></iframe>}
+            
+
         </div>
     )
 }
