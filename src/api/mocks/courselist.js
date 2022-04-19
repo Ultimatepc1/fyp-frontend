40379e76947f4c9bc4fc78e064453c56fb1d1e3a
Priyang <p.c90909@gmail.com>
2022-04-19 22:57:36 +0530

    Courselist reduction  for reduced errors during pilot study

--- src/api/mocks/courselist.js
+++ src/api/mocks/courselist.js
@@ -1,68 +1,81 @@
 export default [{
-    'title' : '1. GET Request',
-    'data' :[
+    'title': '1. GET Request',
+    'data': [
         {
-            'title' : 'Simple GET request without DB',
-            'supportive' : '61fd38d0bf57aeeee4694988',
-            'workedout' : '61fce27ca546db30899924a8',
-            'completion_one':'61f79c01732e939bf6d7baf0',
-            'completion_two':'61f7b9d1732e939bf6d7bb10',
-            'conventional':'61f90720da33014aee14287b',
-        },
-        {
-            'title' : 'Simple GET request with DB',
-            'supportive' : '2',
-            'workedout' : 'id',
-            'completion_one':'id',
-            'completion_two':'id',
-            'conventional':'id',
-        },
-        {
-            'title' : 'Links with dynamic parameter',
-            'supportive' : '3',
-            'workedout' : 'id',
-            'completion_one':'id',
-            'completion_two':'id',
-            'conventional':'id',
-        },
-        {
-            'title' : 'Using query parameters',
-            'supportive' : '4',
-            'workedout' : 'id',
-            'completion_one':'id',
-            'completion_two':'id',
-            'conventional':'id',
-        },
-        {
-            'title' : 'Pagination',
-            'supportive' : '5',
-            'workedout' : 'id',
-            'completion_one':'id',
-            'completion_two':'id',
-            'conventional':'id',
-        },
-        {
-            'title' : 'Data filtering',
-            'supportive' : '6',
-            'workedout' : 'id',
-            'completion_one':'id',
-            'completion_two':'id',
-            'conventional':'id',
-        },
-
-    ]
-},
-{
-    'title' : '2. POST Request',
-    'data' :[
-        {
-            'title' : 'Post Request Without DB',
-            'supportive' : 'id',
-            'workedout' : 'id',
-            'completion_one':'id',
-            'completion_two':'id',
-            'conventional':'id',
+            'title': 'Simple GET request without DB',
+            'supportive': '61fd38d0bf57aeeee4694988',
+            'workedout': '61fce27ca546db30899924a8',
+            'completion_one': '61f79c01732e939bf6d7baf0',
+            'completion_two': '61f7b9d1732e939bf6d7bb10',
+            'conventional': '61f90720da33014aee14287b',
         }
     ]
-}
-] 
+}]
+// export default [{
+//     'title' : '1. GET Request',
+//     'data' :[
+//         {
+//             'title' : 'Simple GET request without DB',
+//             'supportive' : '61fd38d0bf57aeeee4694988',
+//             'workedout' : '61fce27ca546db30899924a8',
+//             'completion_one':'61f79c01732e939bf6d7baf0',
+//             'completion_two':'61f7b9d1732e939bf6d7bb10',
+//             'conventional':'61f90720da33014aee14287b',
+//         },
+//         {
+//             'title' : 'Simple GET request with DB',
+//             'supportive' : '2',
+//             'workedout' : 'id',
+//             'completion_one':'id',
+//             'completion_two':'id',
+//             'conventional':'id',
+//         },
+//         {
+//             'title' : 'Links with dynamic parameter',
+//             'supportive' : '3',
+//             'workedout' : 'id',
+//             'completion_one':'id',
+//             'completion_two':'id',
+//             'conventional':'id',
+//         },
+//         {
+//             'title' : 'Using query parameters',
+//             'supportive' : '4',
+//             'workedout' : 'id',
+//             'completion_one':'id',
+//             'completion_two':'id',
+//             'conventional':'id',
+//         },
+//         {
+//             'title' : 'Pagination',
+//             'supportive' : '5',
+//             'workedout' : 'id',
+//             'completion_one':'id',
+//             'completion_two':'id',
+//             'conventional':'id',
+//         },
+//         {
+//             'title' : 'Data filtering',
+//             'supportive' : '6',
+//             'workedout' : 'id',
+//             'completion_one':'id',
+//             'completion_two':'id',
+//             'conventional':'id',
+//         },
+
+//     ]
+// },
+// {
+//     'title' : '2. POST Request',
+//     'data' :[
+//         {
+//             'title' : 'Post Request Without DB',
+//             'supportive' : 'id',
+//             'workedout' : 'id',
+//             'completion_one':'id',
+//             'completion_two':'id',
+//             'conventional':'id',
+//         }
+//     ]
+// }
+// ] 
